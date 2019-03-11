986d3184a98bf92a6f12388deb8f12738fb00c87
martinhorniktkhk <45164810+martinhorniktkhk@users.noreply.github.com>
2019-03-11 22:34:32 +0200

    Another spec

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,20 +1,19 @@
 describe('Address Book', function() {
-    it('should be able to add a contact', function() {
-        var addressBook = new AddressBook();
+    var addressBook = new AddressBook();
+    thisContact = new Contact();
+
+    beforeEach(function() {
+        addressBok = new AddressBook();
         thisContact = new Contact();
-
-        addressBook.addContact(thisContact);
-
-        expect(addressBook.getContact(0)).toBe(thisContact);
     });
 
+    it('should be able to add a contact', function() {
+        addressBook.addContact(thisContact);
+        expect(addressBook.getContact(0)).toBe(thisContact);
+    });
     it('should be able to delete a contact', function() {
-        var addressBook = new AddressBook(),
-            thisContact = new Contact();
-
         addressBook.addContact(thisContact);
         addressBook.deleteContact(0);
-
         expect(addressBook.getContact(0)).not.toBeDefined();
     });
 
