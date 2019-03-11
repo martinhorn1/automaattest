e14168595f587ef431f433844820f86fa421d7ae
martinhorniktkhk <45164810+martinhorniktkhk@users.noreply.github.com>
2019-03-11 22:30:14 +0200

    Mike's solution

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,3 +1,7 @@
-function AddressBook() {
+AddressBook.prototype.getContact = function(index) {
+    return this.contacts[index];
+}
 
+AddressBook.prototype.deleteContact = function(index) {
+    this.contacts.splice(index, 1);
 }
